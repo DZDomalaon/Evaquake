fddd384ee43d83e31b9d2c825b0e4bf2a3f12667
akaybanez <akaybanez@gmail.com>
2019-11-22 12:29:57 +0800

    Update

--- ThesisItFinal/app/src/main/java/com/example/thesisitfinal/MainActivity.java
+++ ThesisItFinal/app/src/main/java/com/example/thesisitfinal/MainActivity.java
@@ -1,42 +1,70 @@
 package com.example.thesisitfinal;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn_instructions, btn_map, btn_guide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button mapBtn = findViewById(R.id.mapBtn);
-        Button infoBtn = findViewById(R.id.infoBtn);
-        Button guideBtn = findViewById(R.id.guideBtn);
+        btn_instructions = findViewById(R.id.instructionsBtn);
+        btn_map = findViewById(R.id.mapBtn);
+        btn_guide = findViewById(R.id.guideBtn);
 
-        mapBtn.setOnClickListener(new View.OnClickListener() {
+        btn_instructions.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, MapActivity.class));
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), test.class);
+                view.getContext().startActivity(intent);
             }
         });
 
-        infoBtn.setOnClickListener(new View.OnClickListener() {
+        btn_map.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, InfoActivity.class));
-            }
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MapActivity.class);
+                view.getContext().startActivity(intent);}
         });
 
-        guideBtn.setOnClickListener(new View.OnClickListener() {
+        btn_guide.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, GuideActivity.class));
-            }
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), GuideActivity.class);
+                view.getContext().startActivity(intent);}
         });
+
+
     }
+
+    private void showDialog() {
+        //before inflating the custom alert dialog layout, we will get the current activity viewgroup
+        ViewGroup viewGroup = findViewById(android.R.id.content);
+
+        //then we will inflate the custom alert dialog xml that we created
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.dialog, viewGroup, false);
+
+
+        //Now we need an AlertDialog.Builder object
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        //setting the view of the builder to our custom view that we already inflated
+        builder.setView(dialogView);
+
+        //finally creating the alert dialog and displaying it
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+    }
+
 }
