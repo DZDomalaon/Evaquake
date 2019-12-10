597b78ea7c5ae21f56ccdb35896aa1d0c4993462
Darell Domalaon <darelldomalaon@gmail.com>
2019-12-11 02:23:10 +0700

    Marker and User Location
    Manually add location
    Accurate User Location
    Added some navigation codes

--- ThesisItFinal/app/src/main/java/com/example/thesisitfinal/map.java
+++ ThesisItFinal/app/src/main/java/com/example/thesisitfinal/map.java
@@ -1,12 +1,19 @@
 package com.example.thesisitfinal;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.location.LocationComponent;
 import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
@@ -16,68 +23,105 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncherOptions;
+import com.mapbox.services.android.navigation.v5.navigation.MapboxNavigation;
 
+import java.util.ArrayList;
 import java.util.List;
-
 /**
  * Use the LocationComponent to easily add a device location "puck" to a Mapbox map.
  */
 public class map extends AppCompatActivity implements
-        OnMapReadyCallback, PermissionsListener {
+        OnMapReadyCallback, PermissionsListener 
+{
 
     private PermissionsManager permissionsManager;
     private MapboxMap mapboxMap;
     private MapView mapView;
+    private DirectionsRoute currentRoute;
+	private static final String MARKER_SOURCE = "markers-source";
+    private static final String MARKER_STYLE_LAYER = "markers-style-layer";
+    private static final String MARKER_IMAGE = "custom-marker";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
 
-// Mapbox access token is configured here. This needs to be called either in your application
-// object or in the same activity which contains the mapview.
+        // Mapbox access token is configured here. This needs to be called either in your application
+        // object or in the same activity which contains the mapview.
         Mapbox.getInstance(this, getString(R.string.access_token));
-
-// This contains the MapView in XML and needs to be called after the access token is configured.
+        MapboxNavigation navigation = new MapboxNavigation(this, getString(R.string.access_token));
+        // This contains the MapView in XML and needs to be called after the access token is configured.
         setContentView(R.layout.activity_map);
-
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
     }
 
     @Override
-    public void onMapReady(@NonNull final MapboxMap mapboxMap) {
+    public void onMapReady(@NonNull final MapboxMap mapboxMap) 
+	{
         map.this.mapboxMap = mapboxMap;
-
-        mapboxMap.setStyle(new Style.Builder().fromUri("mapbox://styles/mapbox/cjerxnqt3cgvp2rmyuxbeqme7"),
-                new Style.OnStyleLoaded() {
+        map.this.mapboxMap.setMinZoomPreference(15);
+        mapboxMap.setStyle(new Style.Builder().fromUri("mapbox://styles/mapbox/light-v10"),
+                new Style.OnStyleLoaded()
+				{
                     @Override
-                    public void onStyleLoaded(@NonNull Style style) {
+                    public void onStyleLoaded(@NonNull Style style)
+					{
+						style.addImage(MARKER_IMAGE, BitmapFactory.decodeResource(
+                        map.this.getResources(), R.drawable.custom_marker));
+						addMarkers(style);
                         enableLocationComponent(style);
                     }
                 });
     }
+	
+	private void addMarkers(@NonNull Style loadedMapStyle) 
+	{
+        List<Feature> features = new ArrayList<>();
+        features.add(Feature.fromGeometry(Point.fromLngLat(125.6348, 7.1149)));
+        features.add(Feature.fromGeometry(Point.fromLngLat(125.605769, 7.064497)));
+
+        /* Source: A data source specifies the geographic coordinate where the image marker gets placed. */
+
+        loadedMapStyle.addSource(new GeoJsonSource(MARKER_SOURCE, FeatureCollection.fromFeatures(features)));
+
+        /* Style layer: A style layer ties together the source and image and specifies how they are displayed on the map. */
+        loadedMapStyle.addLayer(new SymbolLayer(MARKER_STYLE_LAYER, MARKER_SOURCE)
+                .withProperties(
+                        PropertyFactory.iconAllowOverlap(true),
+                        PropertyFactory.iconIgnorePlacement(true),
+                        PropertyFactory.iconImage(MARKER_IMAGE),
+                        // Adjust the second number of the Float array based on the height of your marker image.
+                        // This is because the bottom of the marker should be anchored to the coordinate point, rather
+                        // than the middle of the marker being the anchor point on the map.
+                        PropertyFactory.iconOffset(new Float[] {0f, -52f})
+                ));
+    }
 
     @SuppressWarnings( {"MissingPermission"})
-    private void enableLocationComponent(@NonNull Style loadedMapStyle) {
-// Check if permissions are enabled and if not request
-        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+    private void enableLocationComponent(@NonNull Style loadedMapStyle)
+    {
+        // Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(this))
+        {
 
-// Get an instance of the component
+            // Get an instance of the component
             LocationComponent locationComponent = mapboxMap.getLocationComponent();
 
-// Activate with options
+            // Activate with options
             locationComponent.activateLocationComponent(
                     LocationComponentActivationOptions.builder(this, loadedMapStyle).build());
-
-// Enable to make component visible
+            // Enable to make component visible
             locationComponent.setLocationComponentEnabled(true);
-
-// Set the component's camera mode
+            // Set the component's camera mode
             locationComponent.setCameraMode(CameraMode.TRACKING);
-
-// Set the component's render mode
+            // Set the component's render mode
             locationComponent.setRenderMode(RenderMode.COMPASS);
         } else {
             permissionsManager = new PermissionsManager(this);
@@ -86,28 +130,43 @@ public class map extends AppCompatActivity implements
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
         permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     @Override
-    public void onExplanationNeeded(List<String> permissionsToExplain) {
+    public void onExplanationNeeded(List<String> permissionsToExplain)
+    {
 
     }
 
     @Override
-    public void onPermissionResult(boolean granted) {
-        if (granted) {
+    public void onPermissionResult(boolean granted)
+    {
+        if (granted)
+        {
             mapboxMap.getStyle(new Style.OnStyleLoaded() {
                 @Override
                 public void onStyleLoaded(@NonNull Style style) {
                     enableLocationComponent(style);
                 }
             });
-        } else {
-
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "Permission not granted", Toast.LENGTH_LONG).show();
             finish();
         }
+    }
+
+    public void navigationBtnClick(View view)
+    {
+        boolean simulateRoute = true;
+        NavigationLauncherOptions options = NavigationLauncherOptions.builder()
+                .directionsRoute(currentRoute)
+                .shouldSimulateRoute(simulateRoute)
+                .build();
     }
 
     @Override
